70ad992154e3c5225e9ceb676dc6ad313b47d692
anoopano <anoopp998@gmail.com>
2021-01-13 17:23:07 +0530

    add bootstrap class

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -6,7 +6,7 @@ function NewsCard({article}) {
     return (
         <Card className="mx-3 newsCard">
             <a href={article.url} >
-                <Card.Img variant="top" src={article.image} />
+                <Card.Img variant="top" src={article.image} className="p-2"/>
                 <Card.Body>
                     <Card.Title><b>{article.title}</b></Card.Title>
                     <Card.Text>
